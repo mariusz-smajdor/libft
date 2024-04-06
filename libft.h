21b83013bfe9daee6ab13b043fb40e8ab5718351
Mariusz Smajdor <101614883+mariusz-smajdor@users.noreply.github.com>
2024-04-06 14:57:10 +0200

    ft_substr function

--- libft.h
+++ libft.h
@@ -29,5 +29,6 @@ char	*ft_strnstr(const char *big, const char *little, size_t len);
 int		ft_atoi(const char *str);
 void	*ft_calloc(size_t count, size_t size);
 char	*ft_strdup(const char *s1);
+char	*ft_substr(const char *s, unsigned int start, size_t len);
 
 #endif
