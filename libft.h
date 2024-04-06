4853d85bfa9d01a8dec1abe225b1e30f422b6f23
Mariusz Smajdor <101614883+mariusz-smajdor@users.noreply.github.com>
2024-04-06 15:36:38 +0200

    ft_itoa function

--- libft.h
+++ libft.h
@@ -33,5 +33,6 @@ char	*ft_substr(const char *s, unsigned int start, size_t len);
 char	*ft_strjoin(const char *s1, const char *s2);
 char	*ft_strtrim(const char *s1, const char *set);
 char	**ft_split(const char *s, char c);
+char	*ft_itoa(int n);
 
 #endif
