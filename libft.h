b0d54ba4fd409c6e082f2f30cf6a11d31634dd4a
Mariusz Smajdor <101614883+mariusz-smajdor@users.noreply.github.com>
2024-04-07 11:07:58 +0200

    ft_putstr_fd function

--- libft.h
+++ libft.h
@@ -38,5 +38,6 @@ char	*ft_itoa(int n);
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 void	ft_striteri(char *s, void (*f)(unsigned int, char*));
 void	ft_putchar_fd(char c, int fd);
+void	ft_putstr_fd(char *str, int fd);
 
 #endif
