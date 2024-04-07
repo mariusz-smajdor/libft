b53ede5f451f72e0faaada35d75df0b5ecaedecd
Mariusz Smajdor <101614883+mariusz-smajdor@users.noreply.github.com>
2024-04-07 11:11:45 +0200

    ft_putendl_fd function

--- libft.h
+++ libft.h
@@ -39,5 +39,6 @@ char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 void	ft_striteri(char *s, void (*f)(unsigned int, char*));
 void	ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char *str, int fd);
+void	ft_putendl_fd(char *str, int fd);
 
 #endif
