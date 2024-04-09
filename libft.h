7a07c2a8d502f3c93ef9cb2246b8eef47a7c3bb0
Mariusz Smajdor <101614883+mariusz-smajdor@users.noreply.github.com>
2024-04-09 18:32:03 +0200

    ft_lstadd_front function

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: msmajdor <msmajdor@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/07 11:21:09 by msmajdor          #+#    #+#             */
-/*   Updated: 2024/04/09 18:29:06 by msmajdor         ###   ########.fr       */
+/*   Updated: 2024/04/09 18:30:58 by msmajdor         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -60,5 +60,6 @@ void	ft_putstr_fd(char *str, int fd);
 void	ft_putendl_fd(char *str, int fd);
 void	ft_putnbr_fd(int n, int fd);
 t_list	*ft_lstnew(void *content);
+void	ft_lstadd_front(t_list **lst, t_list *new);
 
 #endif
