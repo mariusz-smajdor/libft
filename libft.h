62d57631f76e8679ed5a7652ed89c55c122c0b1b
Mariusz Smajdor <101614883+mariusz-smajdor@users.noreply.github.com>
2024-04-10 13:39:57 +0200

    ft_lstclear function

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: msmajdor <msmajdor@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/07 11:21:09 by msmajdor          #+#    #+#             */
-/*   Updated: 2024/04/10 13:28:15 by msmajdor         ###   ########.fr       */
+/*   Updated: 2024/04/10 13:38:12 by msmajdor         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -65,5 +65,6 @@ int		ft_lstsize(t_list *lst);
 t_list	*ft_lstlast(t_list *lst);
 void	ft_lstadd_back(t_list **lst, t_list *new);
 void	ft_lstdelone(t_list *lst, void (*del)(void *));
+void	ft_lstclear(t_list **lst, void (*del)(void *));
 
 #endif
