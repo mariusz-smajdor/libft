92016c82f5f2802ceafe42ca401444f93f18fded
Mariusz Smajdor <101614883+mariusz-smajdor@users.noreply.github.com>
2024-05-04 14:01:43 +0200

    add get_next_line function

--- libft.h
+++ libft.h
@@ -68,5 +68,6 @@ void	ft_lstdelone(t_list *lst, void (*del)(void *));
 void	ft_lstclear(t_list **lst, void (*del)(void *));
 void	ft_lstiter(t_list *lst, void (*f)(void *));
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
+char	*get_next_line(int fd);
 
 #endif
